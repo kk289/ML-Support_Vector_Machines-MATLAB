bb0b31dc66e244d91beeeaff2e8b50a88e636360
kk289 <kevilkhadka@gmail.com>
2020-06-20 17:24:09 -0700

    modify code with own1.txt (spam email)

--- ex6_spam.m
+++ ex6_spam.m
@@ -125,14 +125,14 @@ pause;
 % Set the file to be read in (change this to spamSample2.txt,
 % emailSample1.txt or emailSample2.txt to see different predictions on
 % different emails types). Try your own emails as well!
-filename = 'spamSample1.txt';
+filename = 'own1.txt';
 
 % Read and predict
 file_contents = readFile(filename);
+
 word_indices  = processEmail(file_contents);
 x             = emailFeatures(word_indices);
 p = svmPredict(model, x);
 
 fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
 fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
-
